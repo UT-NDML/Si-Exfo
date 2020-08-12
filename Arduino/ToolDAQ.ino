bf1049d422ea2fe7390986d1e8287b9fad63d61c
mjdubbs <43099120+mjdubbs@users.noreply.github.com>
2020-08-12 18:55:31 -0400

    last arduino update

--- Arduino/ToolDAQ.ino
+++ Arduino/ToolDAQ.ino
@@ -1,31 +1,3 @@
-/*
- Based on ArduinoDRO by Yuriy Krushelnytskiy
- Edits for exfoliation by Martin Ward
- *******************************************
- 
-  ArduinoDRO + Tach V3
-
-  Reading Grizzly iGaging Digital Scales V2.1 Created 19 January 2012
-  Updated 03 April 2013
-  by Yuriy Krushelnytskiy
-  http://www.yuriystoys.com
-
-  Updated 01 June 2014 by Ryszard Malinowski
-  http://www.rysium.com
-
-  This program is free software: you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation, either version 3 of the License, or
-  (at your option) any later version.
-
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
-
-  You should have received a copy of the GNU General Public License
-  along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
 #include "HX711.h"
 #include <Wire.h>
 #include <Encoder.h>
@@ -37,21 +9,6 @@
 
 const int DOUT = 5;
 const int CLK = 4;
-
-// DRO config (if axis is not connected change in the corresponding constant value from "true" to "false")
-boolean const xAxisSupported = true;
-
-// I/O ports config (change pin numbers if DRO, Tach sensor or Tach LED feedback is connected to different ports)
-int const clockPin = 2;
-
-int const xDataPin = 3;
-
-//---END OF CONFIGURATION PARAMETERS ---
-
-boolean const droSupported = (xAxisSupported);
-
-//variables that will store the readout
-volatile long xCoord;
 
 double Setpoint, Input, Output;
 double Kp = .01, Ki = 0.1, Kd = 0.001;
@@ -76,13 +33,9 @@ HX711 scale;
 
 void setup()
 {
-  //clock pin should be set as output
-  pinMode(clockPin, OUTPUT);
-  //data pins should be set as inputs
-  pinMode(xDataPin, INPUT);
   //initialize serial port
   Serial.begin(115200);
-
+  Serial2.begin(115200);
   Setpoint = 0;
   myPID.SetMode(AUTOMATIC);
   myPID.SetOutputLimits(0, 255);
@@ -90,49 +43,17 @@ void setup()
   pinMode(enA, OUTPUT);
   TCCR2B = (TCCR4B & 0xF8) | 0x01;
 
-//  Setpointp = 0;
-//  myPIDp.SetMode(AUTOMATIC);
-//  myPIDp.SetOutputLimits(0, 255);
-//  myPIDp.SetSampleTime(25);
-//  pinMode(PressurePin, OUTPUT);  // sets the pin as output
-//  TCCR4B = (TCCR2B & 0xF8) | 0x01;
-
   average.reset(0.0);
 
   scale.begin(DOUT, CLK);
   scale.set_scale(-206950.0); //This value is obtained by using the SparkFun_HX711_Calibration sketch
   scale.set_offset(-61003);
-
 }
-//float mapfloat(float x, float in_min, float in_max, float out_min, float out_max)
-//{
-//  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
-//}
 void loop()
 {
-  xCoord = 0;
-
-  int bitOffset;
-
-  //read the first 20 bits
-  for (bitOffset = 0; bitOffset < 21; bitOffset++)
-  {
-    tickTock();
-    //read the pin state and shift it into the appropriate variables
-    xCoord |= ((long)digitalRead(xDataPin) << bitOffset);
-  }
-
-  tickTock();
-
-  //read the last bit (signified the sign)
-  //if it's high, fill 11 leftmost bits with "1"s
-  if (digitalRead(xDataPin) == HIGH)
-    xCoord |= ((long)0x7ff << 21);
-
+ 
   Setpoint = map(analogRead(A0), 10, 1023, 0, 3600);
-  //  Setpointp = mapfloat(analogRead(A1), 1, 1023, 0.0, 9.0);
-//  Setpointp = map(analogRead(A1), 1, 1023, 0, 900) / 100.0;
-
+ 
   newPosition = myEnc.read();
   rpm = ((abs(oldPosition - newPosition)) * 60000) / (44 * (millis() - measureTime));
   oldPosition = newPosition;
@@ -141,45 +62,21 @@ void loop()
 
   Serial.print(measureTime);
   Serial.print(" ");
-  Serial.print((long)xCoord);
+//  if (Serial2.read() == 'x') {
+  Serial.print(Serial2.parseInt());
+//  }
   Serial.print(" ");
   Serial.print(Inputp);
-  Serial.print(" ");
+  //  Serial.print(" ");
 //  Serial.print(rpm);
   Serial.println();
 
   noInterrupts();
+  Inputp = scale.get_units(3); //3
   Input = average.get();
-  Inputp = scale.get_units(2); //3
 
   myPID.Compute();
-//  myPIDp.Compute();
-
   analogWrite(enA, Output);
-//  analogWrite(PressurePin, Outputp);
   interrupts();
 
-}
-
-// Clock DRO scales
-inline void tickTock()
-{
-  //tick
-  digitalWrite(clockPin, HIGH);
-
-  //If the scale output is floating all over the place comment lines 99-102 and uncomment line 106.
-
-  //Alternative 1: Use "software" delay (works better on UNO)
-  //give the scales a few microseconds to think about it
-  for (byte i = 0; i < 20; i++)
-  {
-    __asm__("nop\n\t");
-  }
-
-  //Alternative 2: Use proper delay
-  //give the scales a few microseconds to think about it
-  //delayMicroseconds(2);
-
-  //tock
-  digitalWrite(clockPin, LOW);
 }
---
+++ Arduino/nano_encoder.ino
@@ -0,0 +1,189 @@
+/*
+ Based on ArduinoDRO by Yuriy Krushelnytskiy
+ Edits for exfoliation by Martin Ward
+ *******************************************
+ 
+  ArduinoDRO + Tach V3
+
+  Reading Grizzly iGaging Digital Scales V2.1 Created 19 January 2012
+  Updated 03 April 2013
+  by Yuriy Krushelnytskiy
+  http://www.yuriystoys.com
+
+  Updated 01 June 2014 by Ryszard Malinowski
+  http://www.rysium.com
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+  Added support for tachometer on axis T (input pin 7)
+
+  NOTE: This program supports hall-sensor to measure rpm.  The tach output format for Android DRO is T<time>/<retation>.
+  Android DRO application must support this format for axis T.
+
+  Configuration parameters:
+  <n>AxisSupported
+    Defines if DRO functionality on axis <n> should be supported.
+    If supported DRO scale should be connected to I/O pin defined in constant "<n>DataPin" and
+    DRO data is sent to serial port with corresponding axis prefix (X, Y, Z or W)
+    Clock pin is common for all scales should be connected to I/O pin defined in constant "clockPin"
+    Possible values:
+      true = DRO functionality on axis <n> is supported
+      false = DRO functionality on axis <n> is not supported
+    Default value = true
+
+  tachSupported
+    Defines if tach sensor functionality should be supported.
+    If supported tach sensor should be connected to I/O pin defined in constant "tachPin" and
+    rpm value is sent to serial port with axis prefix "T"
+    Possible values:
+      true = tach sensor functionality is supported
+      false = tach sensor functionality is not supported
+    Default value = true
+
+  clockPin
+    Defines the I/O pin where clock signal for all DRO scales is connected
+    Possible values:
+      integer number between 2 and 13
+    Default value = 2
+
+  <n>DataPin
+    Defines the I/O pin where DRO data signal for selected scale is connected
+    Possible values:
+      integer number between 2 and 13
+    Default values = 3, 4, 5, 6 (for corresponding axis X, Y, Z and W)
+
+  tachPin
+    Defines the I/O pin where tach sensor signal is connected
+    Possible values:
+      integer number between 2 and 13
+    Default value = 7
+
+  tachLedFeedbackPin
+    Defines the I/O pin where tach LED feedback is connected.
+    Tach LED feedback indicates the status of tachPin for debugging purposes
+    Possible values:
+      integer number between 2 and 13
+    Default value = 13 (on-board LED)
+
+  minRpmDelay
+    Defines the delay (in milliseconds) in showing 0 when rotation stops.  If rpm is so low and time between tach pulse
+    changes longer than this value, value zero rpm ("T0;") will be sent to the serial port.
+    Note: this number will determine the slowest rpm that can be measured.  In order to measure smaller rpm I suggest
+          to use a sensor with more than one "ticks per revolution" (for example hall sensor with two or more magnets).
+          The number of "ticks per revolution" should be set in tachometer setting in Android app.
+    Possible values:
+      any integer number > 0
+    Default value = 1200 (the minimum rpm measured will be 50 rpm)
+*/
+
+// DRO config (if axis is not connected change in the corresponding constant value from "true" to "false")
+boolean const xAxisSupported = true;
+
+
+// I/O ports config (change pin numbers if DRO, Tach sensor or Tach LED feedback is connected to different ports)
+int const clockPin = 2;
+
+int const xDataPin = 3;
+
+//---END OF CONFIGURATION PARAMETERS ---
+
+boolean const droSupported = (xAxisSupported);
+
+//variables that will store the readout
+volatile long xCoord;
+
+unsigned long tms;
+
+void setup()
+{
+  //clock pin should be set as output
+  if (droSupported)
+    pinMode(clockPin, OUTPUT);
+
+  //data pins should be set as inputs
+  if (xAxisSupported)
+    pinMode(xDataPin, INPUT);
+
+  //initialize serial port
+  Serial.begin(9600);
+}
+
+// The loop function is called in an endless loop
+void loop()
+{
+  tms = millis();
+
+  //readTach() is called so often to provide the greatest accuracy
+  if (droSupported)
+  {
+    xCoord = 0;
+
+    int bitOffset;
+
+    //read the first 20 bits
+    for (bitOffset = 0; bitOffset < 21; bitOffset++)
+    {
+      tickTock();
+
+      //read the pin state and shift it into the appropriate variables
+      if (xAxisSupported)
+        xCoord |= ((long)digitalRead(xDataPin) << bitOffset);
+
+    }
+
+    tickTock();
+
+    //read the last bit (signified the sign)
+    //if it's high, fill 11 leftmost bits with "1"s
+    if (xAxisSupported) {
+      if (digitalRead(xDataPin) == HIGH)
+        xCoord |= ((long)0x7ff << 21);
+    }
+
+    //print DRO positions to the serial port
+//    Serial.print(tms);
+//    Serial.print(" ");
+    if (xAxisSupported) {
+      //Serial.print("X");
+
+      Serial.println((long)xCoord);
+    }
+//    Serial.println();
+  }
+  delay(1);
+}
+
+
+// Clock DRO scales
+inline void tickTock()
+{
+  //tick
+  digitalWrite(clockPin, HIGH);
+
+  //If the scale output is floating all over the place comment lines 99-102 and uncomment line 106.
+
+  //Alternative 1: Use "software" delay (works better on UNO)
+  //give the scales a few microseconds to think about it
+  for (byte i = 0; i < 20; i++)
+  {
+    __asm__("nop\n\t");
+  }
+
+  //Alternative 2: Use proper delay
+  //give the scales a few microseconds to think about it
+  //delayMicroseconds(2);
+
+  //tock
+  digitalWrite(clockPin, LOW);
+}
